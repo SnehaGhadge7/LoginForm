72956ebc7c7ce33de7f839fdf061c182a878661e
snehasghadge77@gmail.com <you@example.com>
2020-07-25 02:13:19 +0530

    CardData mapped using arrow function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
- import React from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginForm from "./app/LoginForm";
 import Card from "./components/Card";
-import CardData from "./components/CardData"
+import CardData from "./components/CardData";
 
-console.log(CardData[2].name);
+const mCard = (val) => {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      name={val.name}
+      link={val.link}
+    />
+  );
+}
 
 function App() {
   return (
     <>
       <div className="main">
-        <Card
-          imgsrc={CardData[0].imgsrc}
-          title={CardData[0].title}
-          name={CardData[0].name}
-          link={CardData[0].link}
-        />
-        <Card
-          imgsrc={CardData[1].imgsrc}
-          title={CardData[1].title}
-          name={CardData[1].name}
-          link={CardData[1].link}
-        />
-        <Card
-          imgsrc={CardData[2].imgsrc}
-          title={CardData[2].title}
-          name={CardData[2].name}
-          link={CardData[2].link}
-        />
-       <Card
-          imgsrc={CardData[3].imgsrc}
-          title={CardData[3].title}
-          name={CardData[3].name}
-          link={CardData[3].link}
-        />
-      </div>
+        {CardData.map(mCard)}
+        </div>
     </>
   );
 }
